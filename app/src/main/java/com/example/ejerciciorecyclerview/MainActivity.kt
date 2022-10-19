83d28a2f48c47cf5aeb1922335389f04b47c0249
djseeba173 <djseeba173@gmail.com>
2022-10-18 21:26:58 -0300

    Aniadido coneccion a firebase pero tiene un problema, voy a tratar de solucionarlo

--- app/src/main/java/com/example/ejerciciorecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciorecyclerview/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.ejerciciorecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+
+        Log.d("hola", "hola soy seba, esto de abajo es la db: ")
+        val miLista = db.collection("rubros").get()
+
+
     }
 }
